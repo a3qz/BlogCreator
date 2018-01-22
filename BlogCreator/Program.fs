6a3649223af0ec9f6a6d4693d3eecf8550abc523
a3qz <rmichale@nd.edu>
2018-01-22 11:19:24 -0500

    cleaned up all but writer

--- BlogCreator/Program.fs
+++ BlogCreator/Program.fs
@@ -10,15 +10,10 @@ open System.Text
 open DocumentFormat.OpenXml
 open System
 
-let _PATH = "C:\\Users\\Ryan\\Documents\\ethicsblogs\\"
-let Main args = 
+// Path to the folder where the blogs are stored
+let BLOG_PATH = "C:\\Users\\Ryan\\Documents\\ethicsblogs\\"
 
-    Directory.GetFiles("C:\Users\Ryan\Documents\ethicsblogs\test", "*") 
-    |> Array.map Path.GetFileName 
-    |> Array.iter (printfn "%s") 
-
-let xmlSeq = Seq.initInfinite (fun index -> sprintf "<author><name>name%d</name><age>%d</age><books><book>book%d</book></books></author>" index index index)
-
+// Create a file based on a sequence of strings and a filename
 let createFile (seq: string seq) numberToTake fileName =
     use streamWriter = new StreamWriter("C:\\Users\\Ryan\\Documents\\" + fileName, false)
     streamWriter.WriteLine("<startTag>")
@@ -30,18 +25,11 @@ let createFile (seq: string seq) numberToTake fileName =
             internalWriter (Seq.skip 1 seq) sw (i-1) endTag)
     internalWriter seq streamWriter numberToTake "</startTag>"
 
-let funcTimer fn =
-    let stopWatch = Stopwatch.StartNew()
-    printfn "Timing started"
-    fn()
-    stopWatch.Stop()
-    printfn "Time elased: %A" stopWatch.Elapsed
 
-        
+// Given an xml element, recursivley traverse it until it finds a value that can be converted to a string
 let rec GetPlainText (element : OpenXmlElement ) : string= 
     let PlainTextInWord = new StringBuilder()
-    let x = 
-        element.Elements() 
+    element.Elements() 
         |> Seq.iter (fun (v: OpenXmlElement) -> match v.LocalName with 
                                                 | "t" -> PlainTextInWord.Append v.InnerText |> ignore
                                                 | "cr" -> PlainTextInWord.Append Environment.NewLine|> ignore
@@ -55,30 +43,25 @@ let rec GetPlainText (element : OpenXmlElement ) : string=
  
     string PlainTextInWord
 
-let fileReader filename = 
-    let header = _PATH
+// Given a filename, return the text of that file as a string
+let FileReader filename = 
+    let header = BLOG_PATH
     let fullname = header + filename
-    let fullname2 = header + "2"
-    let fullname2 = fullname2 + filename
     let byteArray = File.ReadAllBytes fullname
     
     use mem = new MemoryStream() 
     mem.Write(byteArray, 0, (int)byteArray.Length) 
-
-    do
-        use doc = WordprocessingDocument.Open(mem, true) 
-        let sb = new StringBuilder();
-        let bod = doc.MainDocumentPart.Document.Body; 
-        let plaintext = GetPlainText bod
-        printfn "%s" plaintext
-
-    use fs = new FileStream(fullname2, FileMode.Create) 
-    mem.WriteTo(fs) 
-
-
-(funcTimer (fun () -> createFile xmlSeq 100 "file100.xml"))
-Directory.GetFiles(_PATH, "*") 
+    
+    use doc = WordprocessingDocument.Open(mem, true) 
+    let bod = doc.MainDocumentPart.Document.Body; 
+    let plaintext = GetPlainText bod
+    plaintext
+    
+// Effectivley main; get all the files in the directory and call FileReader on them
+Directory.GetFiles(BLOG_PATH, "*") 
     |> Array.map Path.GetFileName 
-    |> Array.iter fileReader 
-    |> ignore
+    |> Array.map FileReader 
+    |> Array.iter (printfn "%s")
+
+// Makes it so that I can read the output without the window closing
 System.Console.ReadLine();
